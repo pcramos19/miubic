8c94b284b390682219efc19be6c7968384fafd4d
Pedro Castañeda <pcramos19@gmail.com>
2020-05-09 13:37:38 +0200

    contact y footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./Footer.scss";
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import GitHubIcon from '@material-ui/icons/GitHub';
-// import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 class Footer extends React.Component {
     render() {
         return(
             <section id="footer" className="footer">
-                {/* <div className="icons">
-                    <a href="https://github.com/pcramos19" target="_blank" rel="noopener noreferrer"><GitHubIcon/></a>
-                    <a href="https://www.linkedin.com/in/pc-ramos/" target="_blank" rel="noopener noreferrer"><LinkedInIcon/></a>
-                    <a href="https://twitter.com/Mascagranzas" target="_blank" rel="noopener noreferrer"><TwitterIcon/></a>
-                </div> */}
-               <p>Copyright © Miubic 2020</p>               
+                <div className="icons">
+                    <a href="https://www.youtube.com/channel/UCe5TSUB_ff9Oh8puuSNVEcQ" target="_blank" rel="noopener noreferrer"><YouTubeIcon/></a>
+                    <a href="https://www.facebook.com/Miubic/" target="_blank" rel="noopener noreferrer"><FacebookIcon/></a>
+                    <a href="https://twitter.com/hashtag/miubic?src=hashtag_click" target="_blank" rel="noopener noreferrer"><TwitterIcon/></a>
+                    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><LinkedInIcon/></a>
+                </div>
+                <div className="terms">
+                    <p>Términos de uso | Política de privacidad | Política de cookies</p>
+                    {/* <p></p> */}
+                </div>
+                <p> ©2020 Copyright Miubic. Todos los derechos reservados.</p>               
             </section>
         )
     }
